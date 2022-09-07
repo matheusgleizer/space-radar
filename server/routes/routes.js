e5f1c1f1f1c82739b85d777d347c66c7cf9aef40
matheusgleizer <matheus@quickcommerce.org>
2022-09-07 18:06:30 +0200

    removes unecessary things

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,16 +1,12 @@
 import express from "express";
-// import getGoogleImage from "../controllers/getGoogleImage.js";
 import getRockets from "../controllers/getRockets.js";
 import getMissions from "../controllers/getMissions.js";
-import cors from "cors";
 import spaceXMiddleware from "../middleware/spaceX.middleware.js";
-// import pixabyMiddleware from "../middleware/pixaby.middleware.js";
-// import getRocketsMiddleware from "../middleware/get-rockets-details.middleware.js";
 
 var router = express.Router();
 
 router.post("/getMissions", spaceXMiddleware, getMissions);
 
-router.post("/getRockets", cors(), spaceXMiddleware, getRockets);
+router.post("/getRockets",  spaceXMiddleware, getRockets);
 
 export default router;
